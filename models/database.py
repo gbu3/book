2b246445b05d7778387df75f4cd032022a83d9c9
gbu3 <gracebu3@gmail.com>
2023-12-09 16:15:16 -0500

    basic skeleton code, reviews done but not tested. need to go back in and change endpoints once finished

--- models/database.py
+++ models/database.py
@@ -30,25 +30,38 @@ class User(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True)
-    name = Column(String)
+    username = Column(String)
+    full_name = Column(String)
+    email = Column(String)
     metadata_info = Column(String)
 
     lists = relationship('List', back_populates='creator')
     reviews = relationship('Review', back_populates='reviewer')
-    liked_lists = relationship('Like', back_populates='user')
+    liked_lists = relationship('List', secondary=like_association, back_populates='liked_by')
 
     # Define a one-to-many relationship for users following this user
-    following = relationship('User', secondary=user_follower_association, back_populates='user')
-    followers = relationship('User', secondary=user_follower_association, back_populates='follower')
-    # following = relationship('UserFollower', foreign_keys=[UserFollower.user_id], back_populates='user')
-    # followers = relationship('UserFollower', foreign_keys=[UserFollower.follower_id], back_populates='follower')
+    following = relationship(
+        'User',
+        secondary=user_follower_association,
+        primaryjoin=user_id == user_follower_association.c.follower,
+        secondaryjoin=user_id == user_follower_association.c.user,
+        back_populates='followers'
+    )
+
+    followers = relationship(
+        'User',
+        secondary=user_follower_association,
+        primaryjoin=user_id == user_follower_association.c.user,
+        secondaryjoin=user_id == user_follower_association.c.follower,
+        back_populates='following'
+    )
 
 class Review(Base):
     __tablename__ = 'reviews'
 
     review_id = Column(Integer, primary_key=True)
-    reviewer_id = Column(Integer, ForeignKey('USERS.user_id'))
-    book_id = Column(Integer, ForeignKey('BOOKS.book_id'))
+    reviewer_id = Column(Integer, ForeignKey('users.user_id'))
+    book_id = Column(Integer, ForeignKey('books.book_id'))
     summary = Column(String)
     note = Column(String)
     rating = Column(Integer)
@@ -57,8 +70,8 @@ class Review(Base):
     book = relationship('Book', back_populates='reviews')
 
 list_book_association = Table('list_books', Base.metadata,
-    Column('list_id', Integer, ForeignKey('Lists.list_id')),
-    Column('book_id', Integer, ForeignKey('Books.book_id'))
+    Column('list_id', Integer, ForeignKey('lists.list_id')),
+    Column('book_id', Integer, ForeignKey('books.book_id'))
 )
 
 class Book(Base):
@@ -75,13 +88,22 @@ class List(Base):
     __tablename__ = 'lists'
 
     list_id = Column(Integer, primary_key=True)
-    creator_id = Column(Integer, ForeignKey('USERS.user_id'))
+    creator_id = Column(Integer, ForeignKey('users.user_id'))
     description = Column(String)
     title = Column(String)
 
     creator = relationship('User', back_populates='lists')
-    liked_by = relationship('Like', back_populates='list')
+    liked_by = relationship('User', secondary=like_association, back_populates='liked_lists')
     books = relationship('Book', secondary=list_book_association, back_populates='lists')
+
+
+if __name__ == '__main__':
+    engine = create_engine(DB_URL)
+            
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
 
 # class ListBook(Base):
 #     __tablename__ = 'list_books'
--- models/reviews.py
+++ models/reviews.py
@@ -7,38 +7,141 @@ from models.database import Base, User, UserFollower, Review, Book, List, ListBo
 
 engine = create_engine(DB_URL)
 
+def _review_to_dict(review):
+    """
+    convert a Review to a dictionary to return
+    """
+    if not review:
+        return None
+    
+    return {
+        "review_id": review.review_id,
+        "reviewer_id": review.reviewer_id,
+        "book_id": review.book_id,
+        "summary": review.summary,
+        "note": review.note,
+        "rating": review.rating
+    }
+
 def get_reviews(user_id, book_id, review_id, limit):
-    pass
+    try:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        query = session.query(Review)
+
+        # parameters are treated like filters
+        if review_id:
+            # if review_id is provided, ignore other filters (just need 1)
+            query = query.filter(Review.review_id == review_id)
+        else:
+            if user_id:
+                query = query.filter(Review.reviewer_id == user_id)
+            if book_id:
+                query = query.filter(Review.book_id == book_id)
+
+        # limit results
+        reviews = query.limit(limit).all()
+
+        return [_review_to_dict(review) for review in reviews]
+    
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        session.close()
 
 def update_review(review_id, user_id, summary, note, rating):
-    pass
+    try:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # check that the user exists
+        user = session.query(User).filter(User.user_id==user_id).first()
+        if not user:
+            print("create review failed: incorrect user id")
+            return None
+
+        review = session.query(Review).filter(Review.review_id==review_id).first()
+
+        if not review:
+            print(f"Review {review_id} does not exist.")
+            return None
+
+        if summary:
+            review.summary = summary
+        
+        if note:
+            review.note = note
+
+        if rating:
+            review.rating = rating
+        
+        session.add(review)
+        session.commit()
+
+        print(f"Review updated: {review.review_id}")
+
+        return _review_to_dict(review)
+    
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        session.close()
 
 def create_review(book_id, user_id, summary, note, rating):
     try:
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        # TODO: add input validation
+        # check that the book is real and that the user exists
+        user = session.query(User).filter(User.user_id==user_id).first()
+        if not user:
+            print("create review failed: incorrect user id")
+            return None
+
+        book = session.query(Book).filter(Book.book_id==book_id).first()
+        if not book:
+            print("create review failed: incorrect book id")
+            return None
 
         # also for the review text, need to enforce the character limit somehow (probably in the frontend)
         # but just in case, here can truncate at the 300th character and only save that
         
-        new_review = Review(reviewer_id=user_id, book_id=book_id, summary=summary, note=note, rating=rating)
+        new_review = Review(reviewer_id=user.user_id, book_id=book.book_id, summary=summary, note=note, rating=rating)
         session.add(new_review)
         session.commit()
 
-        print(f"User created with ID: {new_review.review_id}")
-        session.close()
-        engine.dispose()
+        print(f"New review created: {new_review.review_id}")
 
-        return {"review_id": new_review.review_id, "book_id": new_review.book_id, "user_id": new_review.reviewer_id} # not sure this is the best return type
-
+        return _review_to_dict(new_review)
+    
     except Exception as ex:
         print(ex, file=stderr)
-        session.close()
-        engine.dispose()
         exit(1)
+    finally:
+        session.close()
 
 def delete_review(review_id):
-    pass
+    try:
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # check that the book is real and that the user exists
+        review = session.query(Review).filter(Review.review_id==review_id).first()
+        if review:
+            session.delete(review)
+            session.commit()
+            print(f"Review {review_id} deleted successfully.")
+        else:
+            print(f"No review found with the id {review_id}")
+
+        return review_id
+    
+    except Exception as ex:
+        print(ex, file=stderr)
+        exit(1)
+    finally:
+        session.close()
 
